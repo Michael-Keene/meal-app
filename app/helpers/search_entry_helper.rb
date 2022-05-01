991d1afc88c60501521a876ad426f7ec096bd09d
Michael Keene <koolkeano@hotmail.com>
2022-05-01 20:49:59 +0100

    link searchable to the url needed to post to

--- app/helpers/search_entry_helper.rb
+++ app/helpers/search_entry_helper.rb
@@ -1,2 +1,21 @@
 module SearchEntryHelper
+
+  def create_url(searchable, parent_item)
+    case parent_item
+    when Meal
+      meal_ingredients_url(parent_item, action: :post)
+    when User
+      user_result_to_url(searchable, parent_item)
+    end
+  end
+
+  def user_result_to_url(searchable, user)
+    case searchable
+    when Food
+      food_consumption_event_url(action: :post)
+    when Meal
+      meal_consumption_event_url(action: :post)
+    end
+  end
+
 end

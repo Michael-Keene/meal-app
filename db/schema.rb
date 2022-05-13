499e546426704a13245a5b7680c5a66bf3b273ea
Michael Keene <koolkeano@hotmail.com>
2022-05-13 19:55:50 +0100

    commit schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_220925) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_174914) do
   create_table "food_consumption_events", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "food_id", null: false
     t.float "grams"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_food_consumption_events_on_food_id"
     t.index ["user_id"], name: "index_food_consumption_events_on_user_id"
   end
@@ -29,16 +28,16 @@ ActiveRecord::Schema.define(version: 2022_01_30_220925) do
     t.float "protein"
     t.float "calories"
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
     t.integer "meal_id", null: false
     t.integer "food_id", null: false
     t.float "grams"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_ingredients_on_food_id"
     t.index ["meal_id"], name: "index_ingredients_on_meal_id"
   end
@@ -46,8 +45,8 @@ ActiveRecord::Schema.define(version: 2022_01_30_220925) do
   create_table "meal_consumption_events", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "meal_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["meal_id"], name: "index_meal_consumption_events_on_meal_id"
     t.index ["user_id"], name: "index_meal_consumption_events_on_user_id"
   end
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 2022_01_30_220925) do
   create_table "meals", force: :cascade do |t|
     t.string "name"
     t.string "recipe"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "search_entries", force: :cascade do |t|
@@ -64,21 +63,21 @@ ActiveRecord::Schema.define(version: 2022_01_30_220925) do
     t.string "searchable_type"
     t.string "searchable_name"
     t.string "searchable_text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shopping_lists", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shopping_meal_associations", force: :cascade do |t|
     t.integer "meal_id", null: false
     t.integer "shopping_list_id", null: false
     t.integer "quantity"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["meal_id"], name: "index_shopping_meal_associations_on_meal_id"
     t.index ["shopping_list_id"], name: "index_shopping_meal_associations_on_shopping_list_id"
   end
@@ -87,10 +86,15 @@ ActiveRecord::Schema.define(version: 2022_01_30_220925) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "fat", default: 60.0
+    t.float "carbs", default: 270.0
+    t.float "fibre", default: 30.0
+    t.float "protein", default: 90.0
+    t.float "calories", default: 2000.0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

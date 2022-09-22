65ee91bf0cf76bca1ddf775724293b2d96242a89
Michael Keene <koolkeano@hotmail.com>
2022-09-22 12:42:01 +0100

    correct deleteing meal consumprion events

--- app/presenters/edible_presenters/meal_consumption_event_presenter.rb
+++ app/presenters/edible_presenters/meal_consumption_event_presenter.rb
@@ -16,14 +16,19 @@ module EdiblePresenters
 
     def dom_id(prefix = nil)
       ActionView::RecordIdentifier.dom_id(
-        @meal_consumption_event,
+        meal_consumption_event,
         prefix
       )
     end
 
+    def destroy_path
+      url_helper
+        .meal_consumption_event_path(meal_consumption_event)
+    end
+
     private
 
-    attr_reader :presented_meal
+    attr_reader :presented_meal, :meal_consumption_event
 
   end
 end

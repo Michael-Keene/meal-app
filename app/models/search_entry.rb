946dc2d5f2f1498ed78eab8d735ca7ac384c0b12
Michael Keene <koolkeano@hotmail.com>
2022-10-18 16:21:35 +0100

    add scope for text serach in search entry

--- app/models/search_entry.rb
+++ app/models/search_entry.rb
@@ -3,4 +3,6 @@ class SearchEntry < ApplicationRecord
 
   belongs_to :searchable, polymorphic: true
 
+  scope :with_text, ->(text) { where("searchable_name like '%#{text}%'").or(where("searchable_text like '%#{text}%'"))}
+
 end

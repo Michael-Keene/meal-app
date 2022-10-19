baadd95074fa9e0dd564647c7b72783cf834e821
Michael Keene <koolkeano@hotmail.com>
2022-10-19 17:04:06 +0100

    correct scope

--- app/models/search_entry.rb
+++ app/models/search_entry.rb
@@ -4,7 +4,7 @@ class SearchEntry < ApplicationRecord
   belongs_to :searchable, polymorphic: true
 
   scope :with_text, ->(text) do
-    q = "%#{text}"
+    q = "%#{text}%"
     where("searchable_name like ?", q).or(where("searchable_text like ?", q))
   end
 

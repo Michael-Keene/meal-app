55c44ec27eed3c2956c44266d23206732f8e2d2e
Michael Keene <koolkeano@hotmail.com>
2022-10-19 16:40:28 +0100

    protect against sql injection

--- app/models/search_entry.rb
+++ app/models/search_entry.rb
@@ -3,6 +3,9 @@ class SearchEntry < ApplicationRecord
 
   belongs_to :searchable, polymorphic: true
 
-  scope :with_text, ->(text) { where("searchable_name like '%#{text}%'").or(where("searchable_text like '%#{text}%'"))}
+  scope :with_text, ->(text) do
+    q = "%#{text}"
+    where("searchable_name like ?", q).or(where("searchable_text like ?", q))
+  end
 
 end
